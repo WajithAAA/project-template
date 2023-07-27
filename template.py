ce2dd7d4317b09494458c2afffbf3f151687208a
WajithAAA <azeeswajid@gmail.com>
2023-07-27 16:58:39 +0530

    upload template

---
+++ template.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(levelname)s: %(message)s')
+
+project_name = "eddie_ai"
+
+list_of_files = [
+    ".github/workflow/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "developer_guide/developer_guide.md",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockeerfile",
+    "setup.py",
+    "requirements.txt",
+    "research/trails.ipynb",
+
+    "app/__init__.py",
+    "app/hub.py",
+    "app/auth/__init__.py",
+    "app/auth/views.py",
+    "app/main/__init__.py",
+    "app/main/views.py",
+    "app/templates/index.html",
+    "app/static/style.css",
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info("Creating directory: {} for the file {}".format(filedir, filename))
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)) == 0:
+        with open(filepath, "w") as f:
+            pass
+            logging.info("Creating empty file: {}".format(filepath))
+
+    else:
+        logging.info("{} is already exists".format(filename))
+
+
